f6b53521c50e161e90f22756d457bf5e12b181b3
Herve Desrosiers <hdesrosiers@stingray.com>
2020-08-13 09:15:39 -0400

    step 1. added lifecycle-extensions dependency to create GameViewModel and used ViewModelProvider to link it to GameFragment controller

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,11 +17,13 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -42,6 +44,8 @@ class GameFragment : Fragment() {
 
     private lateinit var binding: GameFragmentBinding
 
+    private lateinit var gameViewModel: GameViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
@@ -55,6 +59,9 @@ class GameFragment : Fragment() {
 
         // TODO (04) Create and initialize a GameViewModel, using ViewModelProviders; Add a log
         // statement
+
+        Log.i("GameViewModel", "Called ViewModelProvider")
+        gameViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         resetList()
         nextWord()
