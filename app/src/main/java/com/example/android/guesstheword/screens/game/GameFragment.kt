ba5a5a478da2aeb2ec3bc81fbc3bd08dabd60db6
Herve Desrosiers <hdesrosiers@stingray.com>
2020-08-15 21:37:18 -0400

    step 8. added ViewModel data binding variables to layouts to handle clicks there and remove listeners in fragments

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -54,12 +54,7 @@ class GameFragment : Fragment() {
 
         // TODO (03) Pass the GameViewModel into the data binding - then you can remove the
         // OnClickListener setup from here
-        binding.correctButton.setOnClickListener {
-            viewModel.onCorrect()
-        }
-        binding.skipButton.setOnClickListener {
-            viewModel.onSkip()
-        }
+        binding.gameViewModel = viewModel
 
         /** Setting up LiveData observation relationship **/
         viewModel.word.observe(this, Observer { newWord ->
